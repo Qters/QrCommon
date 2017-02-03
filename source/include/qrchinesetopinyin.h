b6d36256e4ed532b1539ad0940c41bab8b2784a0
yoer <iamyoer@gmail.com>
2017-02-03 16:13:36 +0800

    chinese to pinyin

---
+++ source/include/qrchinesetopinyin.h
@@ -0,0 +1,43 @@
+﻿/**
+  *	@brief
+  *	@author	adam
+  *	@date	2016/7/12
+  */
+
+
+#ifndef CHINESETOPINYIN_H
+#define CHINESETOPINYIN_H
+
+#include <string>
+
+#include "qrcommon_global.h"
+
+NS_QRCOMMON_BEGIN
+
+class QRCOMMONSHARED_EXPORT QrChineseToPinYin
+{
+public:
+    /*!
+     * \brief convert
+     * \param chinese
+     * \param onlyHead
+     * \return
+     */
+    static std::string convert(const std::wstring &chinese, bool onlyHead = false);
+
+private:
+    static long searchUnicodePinYinIdx(unsigned long key);
+
+private:
+    typedef struct
+    {
+        signed long dwCode;
+        const char *szText;
+    }ST_PinyinData;
+
+    static ST_PinyinData sPinYinData[];
+};
+
+NS_QRCOMMON_END
+
+#endif // CHINESETOPINYIN_H
